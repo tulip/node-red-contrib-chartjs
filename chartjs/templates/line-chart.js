918d325e833974d8380f49c93d2c14fcf025fc6e
Kathy Camenzind <kathy.camenzind@tulip.co>
2021-07-07 12:11:49 -0400

    Complete chartjs 3 migration  & documentation

--- chartjs/templates/line-chart.js
+++ chartjs/templates/line-chart.js
@@ -1,170 +1,172 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    // resize canvas to screen
-    function resizeCanvas() {
-        $("#container").css({
-            "height": window.innerHeight - $("#toolbar").outerHeight(),
-            "width": window.innerWidth
-        });
+document.addEventListener("DOMContentLoaded", function (event) {
+  // resize canvas to screen
+  function resizeCanvas() {
+    $("#container").css({
+      height: window.innerHeight - $("#toolbar").outerHeight(),
+      width: window.innerWidth,
+    });
+  }
+
+  $(window).resize(function () {
+    resizeCanvas();
+  });
+
+  resizeCanvas();
+
+  // implement message topic event
+  const topic = window.location.pathname.replace("/", "");
+
+  // connect to socket.io server
+  const socket = io.connect(window.location.origin, {
+    query: "topic=" + topic,
+  });
+
+  socket.on(topic, function (red) {
+    console.log(red);
+
+    // update chart configuration
+    if (red.config !== undefined) {
+      defaultOptions.plugins.title.text = red.config.title;
+      defaultOptions.scales.x.title.text = red.config.xaxis;
+      defaultOptions.scales.y.title.text = red.config.yaxis;
     }
 
-    $(window).resize(function() {
-        resizeCanvas();
-    });
+    if (red.options !== undefined) {
+      try {
+        let new_options = red.options;
+        let mergedOptions = _.merge(defaultOptions, new_options);
+        config.options = mergedOptions;
+      } catch (err) {
+        console.log("Error parsing other options for chart:", err);
+      }
+    }
 
-    resizeCanvas();
+    // update chart dataset
+    if (red.payload !== undefined) {
+      if (!Array.isArray(red.payload)) {
+        const payload = [];
+        payload.push(red.payload);
 
-    // implement message topic event
-    var topic = window.location.pathname.replace('/', '');
+        red.payload = payload;
+      }
 
-    // connect to socket.io server
-    var socket = io.connect(window.location.origin, {query:'topic=' + topic});
+      // initialize graph labels datasets
+      // NOTES: the several series must have the same x axis distribution
+      // get the first serial x axis
+      chart.config.data.labels = [];
 
-    socket.on(topic, function(red){
-        console.log(red);
+      red.payload[0].dataset.forEach((item) => {
+        chart.config.data.labels.push(item.x);
+      });
 
-        // update chart dataset
-        if (red.msg !== undefined) {
-            if (!Array.isArray(red.msg.payload)) {
-              var payload = [];
-              payload.push(red.msg.payload);
+      chart.config.data.datasets = [];
 
-              red.msg.payload = payload;
-            }
+      red.payload.forEach((serie, i) => {
+        const dataset = {
+          label: serie.channel,
+          backgroundColor: serie.color,
+          borderColor: serie.color,
+          data: [],
+          fill: false,
+        };
 
-            // initialize graph labels datasets
-            // NOTES: the several series must have the same x axis distribution
-            // get the first serial x axis
-            chart.config.data.labels = [];
+        serie.dataset.forEach((item) => {
+          dataset.data.push(item.y);
+        });
 
-            red.msg.payload[0].dataset.forEach(item => {
-                chart.config.data.labels.push(item.x);
-            });
+        chart.config.data.datasets.push(dataset);
+      });
+    }
 
-            chart.config.data.datasets = [];
+    console.log(config);
 
-            red.msg.payload.forEach((serie, i) => {
-              var dataset = {
-                  label: serie.channel,
-                  backgroundColor: serie.color,
-                  borderColor: serie.color,
-                  data: [],
-                  fill: false
-              };
+    // refresh chart
+    chart.update();
+  });
 
-              serie.dataset.forEach(item => {                  
-                  dataset.data.push(item.y);
-              });
+  // export event
+  $(".dropdown-menu").on("click", "a", function (event) {
+    // set a new white canvas to be exported
+    destinationCanvas = document.createElement("canvas");
+    destinationCanvas.width = canvas.width;
+    destinationCanvas.height = canvas.height;
 
-              chart.config.data.datasets.push(dataset);
-            });
+    const ctx = destinationCanvas.getContext("2d");
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(canvas, 0, 0);
 
-            console.log("chart", chart)
+    const canvasImg = destinationCanvas.toDataURL("image/png", 1.0);
 
-            // refresh chart
-            chart.update();
-        }
+    // export to image or pdf file
+    if (event.target.id == "image") {
+      const link = document.createElement("a");
 
-        // update chart configuration
-        if (red.config !== undefined) {
-            config.options.plugins.title.text = red.config.title;
-            config.options.scales.x.title.text = red.config.xaxis;
-            config.options.scales.y.title.text = red.config.yaxis;
-            try {
-                if(red.config.options){
-                    let other_options = JSON.parse(red.config.options);
-                    let new_options = _.merge(config.options, other_options)
-                    config.options = new_options
-                }
-            }
-            catch(err) {
-                console.log("Error parsing other options for chart:", err);
-            }
-            // refresh chart
-            chart.update();
-        }
-    });
+      link.download = "image";
+      link.href = canvasImg;
+      link.click();
+    } else {
+      const doc = new jsPDF("landscape");
 
-    // export event
-    $(".dropdown-menu").on("click", "a", function(event) {
-        // set a new white canvas to be exported
-        destinationCanvas = document.createElement("canvas");
-        destinationCanvas.width = canvas.width;
-        destinationCanvas.height = canvas.height;
+      doc.addImage(canvasImg, "JPEG", 10, 10, 280, 110);
+      doc.save("canvas.pdf");
+    }
+  });
 
-        var ctx = destinationCanvas.getContext('2d');
-        ctx.fillStyle = 'white';
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-        ctx.drawImage(canvas, 0, 0);
+  // get canvas chart
+  const canvas = document.getElementById("chart");
+  const ctx = canvas.getContext("2d");
 
-        var canvasImg = destinationCanvas.toDataURL('image/png', 1.0);
-
-        // export to image or pdf file
-        if (event.target.id == 'image') {
-            var link = document.createElement('a')
-
-            link.download = 'image'
-            link.href = canvasImg
-            link.click()
-        }
-        else {
-            var doc = new jsPDF('landscape');
-
-            doc.addImage(canvasImg, 'JPEG', 10, 10, 280, 110 );
-	        doc.save('canvas.pdf');
-        }
-    });
-
-    // get canvas chart
-    var canvas = document.getElementById('chart');
-    var ctx = canvas.getContext('2d');
-
-    // configure chart
-    var config = {
-        type: 'line',
-        data: {
-            labels: [],
-            datasets: []
+  const defaultOptions = {
+    responsive: true,
+    animation: false,
+    maintainAspectRatio: false,
+    plugins: {
+      title: {
+        display: true,
+        text: "Line Chart",
+      },
+    },
+    tooltips: {
+      mode: "index",
+      intersect: false,
+    },
+    hover: {
+      mode: "nearest",
+      intersect: true,
+    },
+    scales: {
+      x: {
+        display: true,
+        title: {
+          display: true,
+          text: "Item",
         },
-        options: {
-            responsive: true,
-            maintainAspectRatio: false,
-            plugins: {
-                title: {
-                    display: true,
-                    text: 'Line Chart'
-                }
-            },
-            tooltips: {
-                mode: 'index',
-                intersect: false,
-            },
-            hover: {
-                mode: 'nearest',
-                intersect: true
-            },
-            scales: {
-                x: {
-                    display: true,
-                    title: {
-                        display: true,
-                        text: 'Item'
-                    },
-                    type: "linear"
-                },
-                y: {
-                    display: true,
-                    title: {
-                        display: true,
-                        text: 'Value'
-                    }
-                }
-            }
-        }
-    };
+        type: "linear",
+      },
+      y: {
+        display: true,
+        title: {
+          display: true,
+          text: "Value",
+        },
+      },
+    },
+  };
 
-    // define global Chart Options and create chart
-    Chart.defaults.defaultFontColor = 'grey';
-    Chart.defaults.defaultFontSize = 16;
+  // configure chart
+  const config = {
+    type: "line",
+    data: {
+      labels: [],
+      datasets: [],
+    },
+    options: defaultOptions,
+  };
 
-    var chart = new Chart(ctx, config);
+  // define global Chart Options and create chart
+  Chart.defaults.defaultFontColor = "grey";
+  Chart.defaults.defaultFontSize = 16;
+
+  let chart = new Chart(ctx, config);
 });
--- chartjs/templates/radar-chart.js
+++ chartjs/templates/radar-chart.js
@@ -1,148 +1,150 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    // resize canvas to screen
-    function resizeCanvas() {
-        $("#container").css({
-            "height": window.innerHeight - $("#toolbar").outerHeight(),
-            "width": window.innerWidth
-        });
+document.addEventListener("DOMContentLoaded", function (event) {
+  // resize canvas to screen
+  function resizeCanvas() {
+    $("#container").css({
+      height: window.innerHeight - $("#toolbar").outerHeight(),
+      width: window.innerWidth,
+    });
+  }
+
+  $(window).resize(function () {
+    resizeCanvas();
+  });
+
+  resizeCanvas();
+
+  const topic = window.location.pathname.replace("/", "");
+
+  // connect to socket.io server
+  const socket = io.connect(window.location.origin, {
+    query: "topic=" + topic,
+  });
+
+  socket.on(topic, function (red) {
+    console.log(red);
+
+    // update chart configuration
+    if (red.config !== undefined) {
+      defaultOptions.plugins.title.text = red.config.title;
+      defaultOptions.scales.x.title.text = red.config.xaxis;
+      defaultOptions.scales.y.title.text = red.config.yaxis;
     }
 
-    $(window).resize(function() {
-        resizeCanvas();
-    });
+    if (red.options !== undefined) {
+      try {
+        let new_options = red.options;
+        let mergedOptions = _.merge(defaultOptions, new_options);
+        config.options = mergedOptions;
+      } catch (err) {
+        console.log("Error parsing other options for chart:", err);
+      }
+    }
 
-    resizeCanvas();
+    // update chart dataset
+    if (red.payload !== undefined) {
+      const dataset = {
+        label: red.payload.channel,
+        backgroundColor: red.payload.color,
+        borderColor: red.payload.color,
+        data: [],
+        fill: false,
+      };
 
-    var topic = window.location.pathname.replace('/', '');
+      chart.config.data.labels = [];
+      chart.config.data.datasets = [];
 
-    // connect to socket.io server
-    var socket = io.connect(window.location.origin, {query:'topic=' + topic});
-
-    socket.on(topic, function(red){
-        console.log(red);
-
-        // update chart dataset
-        if (red.msg !== undefined) {
-            var dataset = {
-                label: red.msg.payload.channel,                        
-                backgroundColor: red.msg.payload.color,
-                borderColor: red.msg.payload.color,
-                data: [],
-                fill: false
-            };
-
-            chart.config.data.labels = [];
-            chart.config.data.datasets = [];
-
-            red.msg.payload.dataset.forEach(item => {
-                chart.config.data.labels.push(item.x);
-                dataset.data.push(item.y);
-            });
-                    
-            chart.config.data.datasets.push(dataset);
-
-            // refresh chart
-            chart.update();
-        }
-
-        // update chart configuration
-        if (red.config !== undefined) {            
-            config.options.title.text = red.config.title;
-            config.options.scales.x.title.text = red.config.xaxis;
-            config.options.scales.y.title.text = red.config.yaxis;
-            try {
-                if(red.config.options){
-                    let other_options = JSON.parse(red.config.options);
-                    let new_options = _.merge(config.options, other_options)
-                    config.options = new_options
-                }
-            }
-            catch(err) {
-                console.log("Error parsing other options for chart:", err);
-            }
-            
-            // refresh chart
-            chart.update();
-        }
+      red.payload.dataset.forEach((item) => {
+        chart.config.data.labels.push(item.x);
+        dataset.data.push(item.y);
       });
 
-    // export event
-    $(".dropdown-menu").on("click", "a", function(event) {
-        // set a new white canvas to be exported
-        destinationCanvas = document.createElement("canvas");
-        destinationCanvas.width = canvas.width;
-        destinationCanvas.height = canvas.height;
+      chart.config.data.datasets.push(dataset);
+    }
 
-        var ctx = destinationCanvas.getContext('2d');
-        ctx.fillStyle = 'white';
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-        ctx.drawImage(canvas, 0, 0);
+    // refresh chart
+    chart.update();
+  });
 
-        var canvasImg = destinationCanvas.toDataURL('image/png', 1.0);
+  // export event
+  $(".dropdown-menu").on("click", "a", function (event) {
+    // set a new white canvas to be exported
+    destinationCanvas = document.createElement("canvas");
+    destinationCanvas.width = canvas.width;
+    destinationCanvas.height = canvas.height;
 
-        // export to image or pdf file
-        if (event.target.id == 'image') {
-            var link = document.createElement('a')
+    const ctx = destinationCanvas.getContext("2d");
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(canvas, 0, 0);
 
-            link.download = 'image'
-            link.href = canvasImg
-            link.click()            
-        }
-        else {
-            var doc = new jsPDF('landscape');
-            
-            doc.addImage(canvasImg, 'JPEG', 10, 10, 280, 110 );
-	        doc.save('canvas.pdf');
-        }
-    });
+    const canvasImg = destinationCanvas.toDataURL("image/png", 1.0);
 
-    // get canvas chart
-    var canvas = document.getElementById('chart'); 
-    var ctx = canvas.getContext('2d');
+    // export to image or pdf file
+    if (event.target.id == "image") {
+      const link = document.createElement("a");
 
-    var config = {
-        type: 'radar',
-        data: {
-            labels: [],
-            datasets: []
+      link.download = "image";
+      link.href = canvasImg;
+      link.click();
+    } else {
+      const doc = new jsPDF("landscape");
+
+      doc.addImage(canvasImg, "JPEG", 10, 10, 280, 110);
+      doc.save("canvas.pdf");
+    }
+  });
+
+  // get canvas chart
+  const canvas = document.getElementById("chart");
+  const ctx = canvas.getContext("2d");
+
+  const defaultOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      title: {
+        display: true,
+        text: "Radar Chart",
+      },
+    },
+    tooltips: {
+      mode: "index",
+      intersect: false,
+    },
+    hover: {
+      mode: "nearest",
+      intersect: true,
+    },
+    scales: {
+      x: {
+        display: true,
+        title: {
+          display: true,
+          text: "Item",
         },
-        options: {
-            responsive: true,
-            maintainAspectRatio: false,
-            title: {
-                display: true,
-                text: 'Radar Chart'
-            },
-            tooltips: {
-                mode: 'index',
-                intersect: false,
-            },
-            hover: {
-                mode: 'nearest',
-                intersect: true
-            },
-            scales: {
-                x: {
-                    display: true,
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Item'
-                    },
-                },
-                y: {
-                    display: true,
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Value'
-                    }
-                }
-            }
-        }
-    };
+      },
+      y: {
+        display: true,
+        title: {
+          display: true,
+          text: "Value",
+        },
+      },
+    },
+  };
 
-    // Global Chart Options
-    Chart.defaults.defaultFontColor = 'grey';
-    Chart.defaults.defaultFontSize = 16;
-        
-    var chart = new Chart(ctx, config);	
+  const config = {
+    type: "radar",
+    data: {
+      labels: [],
+      datasets: [],
+    },
+    options: defaultOptions,
+  };
+
+  // Global Chart Options
+  Chart.defaults.defaultFontColor = "grey";
+  Chart.defaults.defaultFontSize = 16;
+
+  let chart = new Chart(ctx, config);
 });
